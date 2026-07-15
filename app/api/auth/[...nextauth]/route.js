013b8fb0f99f512a84b6fc200a0b1b849f2b48bd
ElectroBillen <info@electrobillen.be>
2026-07-15 13:06:40 +0200

    Implement Google authentication with NextAuth

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,1 +1,21 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          scope:
+            "openid email profile https://www.googleapis.com/auth/calendar",
+          access_type: "offline",
+          prompt: "consent",
+        },
+      },
+    }),
+  ],
+});
+
+export { handler as GET, handler as POST };
