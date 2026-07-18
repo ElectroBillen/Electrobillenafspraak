9bbd9020c1a4488a29f9fc5281b1d37521e2cafd
ElectroBillen <info@electrobillen.be>
2026-07-18 15:27:47 +0200

    Add metadata for appointment booking layout

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,12 @@
+export const metadata = {
+  title: "Afspraak maken",
+  description: "Boek eenvoudig een afspraak"
+};
 
+export default function RootLayout({ children }) {
+  return (
+    <html lang="nl">
+      <body>{children}</body>
+    </html>
+  );
+}
